dbc42aab9be136a72f3cf1331692e84af396ad99
linesh <linesh.simpcity@gmail.com>
2016-08-18 23:27:12 +0800

    Linesh: Add script to extract keymap data from markdown

---
+++ automation-scripts/extract-keymap-from-article.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const sample = fs.readFileSync('sample.md', 'utf-8').toString()
+
+extractKeymapData(sample)
+
+function sanityCheck(sample) {
+
+}
+
+function extractKeymapData(sample) {
+    const all_keymaps = sample.match(/^\|.*\|$/gm).filter(line => !line.includes('---')) // format line
+                                .filter(line => !line.includes('Operation'))        // table header line
+                                .filter(line => line.replace(/(\|)*/, '').length > 0) // separator
+
+    const indecies = sample.match(/#{3,4}.*/gm).map(section => {
+        return {
+            title: /#*\s*(.*)（/gm.exec(section)[1],
+            index: sample.indexOf(section)
+        }
+    })
+    const sections = []
+    for (let i = 0; i < indecies.length - 1; i++) {
+        const content = sample.substring(indecies[i].index, indecies[i + 1].index)
+        sections.push({
+            title: indecies[i].title,
+            content: content
+        })
+    }
+    sections.push({
+        title: indecies[indecies.length - 1].title,
+        content: sample.substring(indecies[indecies.length - 1], sample.length)
+    })
+
+    const keymaps = {}
+    sections.forEach(section => keymaps[section.title] = [])
+    all_keymaps.forEach(keymap => {
+        const belongs = sections.filter(section => section.content.includes(keymap))[0].title
+        keymaps[belongs].push(keymap.match(/.*/g))
+    })
+
+    console.log(keymaps)
+}
